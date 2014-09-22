78bde61fa9091ab7e6a4202870dc674d97a4c1a9
Momo <amaury.darcet@polytechnique.edu>
2014-09-22 17:48:02 -0400

    1

--- predict.py
+++ predict.py
@@ -9,23 +9,25 @@ n_estimators = 300
 
 number = {'Dog' : 5,
           'Patient' : 2}
-for race in ('Dog', 'Patient'):
-    for j in range(1, number[race]+1):
-        subject = data.Subject(race, j)
+with open('submission.txt','w') as f:
+    f.write('clip,preictal')
+    for race in ('Dog', 'Patient'):
+        for j in range(1, number[race]+1):
+            subject = data.Subject(race, j)
 
-        clf = pickle.load(open('pickles/rf_clf_%s_%s_%s.p' % (n_estimators, race, j), 'r'))
+            clf = pickle.load(open('pickles/rf_clf_%s_%s_%s.p' % (n_estimators, race, j), 'r'))
 
-        for segment in subject.segments:
-            if segment.type == 'test':
-                X = []
-                segment.loadData()
-                n = segment.point(10)
-                i=0
-                while (i+1)*n<=segment.length:
-                    X.append(np.abs(np.fft.rfft(segment[i*n:(i+1)*n].data, axis = 1)[:,:80].flatten()))
-                    i += 1
-                segment.data = None
-                print os.path.basename(segment.filepathes[0]), clf.predict_proba(X)[:,1].mean()
+            for segment in subject.segments:
+                if segment.type == 'test':
+                    X = []
+                    segment.loadData()
+                    n = segment.point(10)
+                    i=0
+                    while (i+1)*n<=segment.length:
+                        X.append(np.abs(np.fft.rfft(segment[i*n:(i+1)*n].data, axis = 1)[:,:80].flatten()))
+                        i += 1
+                    segment.data = None
+                    f.write('%s,%s\n' %(os.path.basename(segment.filepathes[0]), clf.predict_proba(X)[:,1].mean()))
 
 
 
